eecf114891d07eb4fb93adb7b44f391f76747b10
Kyle <kyleworkman15@augustana.edu>
2016-08-31 13:44:45 -0500

    Started the new Yak application and printed bye.

--- TestProject/TestMain.java
+++ TestProject/TestMain.java
@@ -2,7 +2,8 @@
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Emu World");
+		System.out.println("Hello Emu World");		
+		System.out.println("Goodbye.");
 	}
 
 }
